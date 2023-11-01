a411b82ee7a409096f1c8ffcea08efb6bb7c4140
Khurshida-Meem <meemrumi59@gmail.com>
2023-11-01 13:05:29 +0600

    created grid layout

--- src/components/body.js
+++ src/components/body.js
@@ -1,11 +1,16 @@
-import React from 'react'
+import React from "react";
+import { imageArr } from "../utils/imageArr";
 
 const Body = () => {
   return (
-    <div>
-        
+    <div className="grid_container">
+      {imageArr?.map((singleImg, index) =>(
+        <div draggable className={`grid_item ${index ===0 && "grid_item_featured"}`} key={singleImg?.id}>
+            <img src={singleImg?.img} alt=""/>
+        </div>
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default Body
+export default Body;
