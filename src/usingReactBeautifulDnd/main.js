766109feb14fa07e6b6d2c9ee21a1437010c3784
Khurshida Meem <meem@ibos.io>
2023-11-02 12:13:44 +0600

    ad-hoc change

--- src/usingReactBeautifulDnd/main.js
+++ src/usingReactBeautifulDnd/main.js
@@ -2,6 +2,20 @@ import React, { useState } from "react";
 import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
 import { imageArr } from "../utils/imageArr";
 
+// const getListStyle = (isDraggingOver) => ({
+//   background: isDraggingOver ? "lightblue" : "lightgrey",
+//   padding: grid,
+//   width: 250,
+// });
+
+// const getItemStyle = (isDragging, draggableStyle) => ({
+//   userSelect: "none",
+//   padding: grid * 2,
+//   margin: `0 0 ${grid}px 0`,
+//   background: isDragging ? "lightgreen" : "grey",
+//   ...draggableStyle,
+// });
+
 const Main = () => {
   const [images, setImages] = useState(imageArr);
 
@@ -30,7 +44,7 @@ const Main = () => {
   return (
     <div className="container">
       <DragDropContext onDragEnd={onDragEnd}>
-        <Droppable droppableId="droppable" direction="horizontal">
+        <Droppable droppableId="droppable" direction="both">
           {(provided, snapshot) => (
             <div
               className="grid_container"
@@ -38,18 +52,27 @@ const Main = () => {
               {...provided.droppableProps}
             >
               {images.map((item, index) => (
-                <Draggable key={item.id} draggableId={item.id.toString()} index={index}>
+                <Draggable
+                  key={item.id}
+                  draggableId={item.id.toString()}
+                  index={index}
+                >
                   {(provided, snapshot) => (
                     <div
                       className={`grid_item ${
                         index === 0 && "grid_item_featured"
                       }`}
+                      // style={getItemStyle(
+                      //   snapshot.isDragging,
+                      //   provided.draggableProps.style
+                      // )}
                       ref={provided.innerRef}
                       {...provided.draggableProps}
                       {...provided.dragHandleProps}
                     >
-                        {/* hi */}
-                      <img src={item?.img} alt="" />
+                      drag the div {item.id}
+                      {/* hi */}
+                      {/* <img src={item?.img} alt="" /> */}
                     </div>
                   )}
                 </Draggable>
